a77987a1505aac8c1b32e6e7ec2ac4598939fe8f
Neptune27 <vominhtri13@gmail.com>
2023-03-15 19:26:32 +0700

    Add database, mysql connector.

---
+++ sql_query/Drop Database.sql
@@ -0,0 +1,39 @@
+USE LIBRARY_MANAGEMENT;
+drop table if exists BOOK_AUTHOR;
+
+drop table if exists AUTHOR;
+
+drop table if exists BOOK_GENRE;
+
+drop table if exists BOOK_PUBLISHER;
+
+drop table if exists BORROW_BOOK_TICKET_FAULT;
+
+drop table if exists BOOK_FAULT;
+
+drop table if exists BORROW_TICKET_DETAILS;
+
+drop table if exists BORROW_TICKET;
+
+drop table if exists EMPLOYEE;
+
+drop table if exists EMPLOYEE_PERMISSION;
+
+drop table if exists GENRE;
+
+drop table if exists MEMBERSHIP;
+
+drop table if exists MEMBERSHIP_TYPE;
+
+drop table if exists PUBLISHER;
+
+drop table if exists SELL_TICKET_DETAILS;
+
+drop table if exists BOOK;
+
+drop table if exists ROOM;
+
+drop table if exists SELL_TICKET;
+
+drop table if exists CUSTOMER;
+
