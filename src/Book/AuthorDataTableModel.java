cff1ae9df178a0310c0b70c84cad839c09402ec2
Neptune27 <vominhtri13@gmail.com>
2023-04-08 07:52:46 +0700

    .

---
+++ src/Book/AuthorDataTableModel.java
@@ -0,0 +1,31 @@
+package Book;
+
+import Utils.AbstractTableModelWithFilters;
+
+public class AuthorDataTableModel extends AbstractTableModelWithFilters<Author> {
+
+    public AuthorDataTableModel() {
+        String[] cols = {"Id", "Tên", "Giới Tính", "Email", "Mô Tả"};
+    }
+    @Override
+    public Object translateValue(Author value, int columnIndex) {
+        switch (columnIndex) {
+            case 0 -> {
+                return value.getId();
+            }
+            case 1 -> {
+                return value.getName();
+            }
+            case 2 -> {
+                return value.getGender().toString();
+            }
+            case 3 -> {
+                return value.getEmail();
+            }
+            case 4 -> {
+                return value.getDescription();
+            }
+        }
+        return null;
+    }
+}
--- src/Utils/AbstractTableModelWithFilters.java
+++ src/Utils/AbstractTableModelWithFilters.java
@@ -7,10 +7,51 @@ import javax.swing.text.JTextComponent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class AbstractTableModelWithFilters<T> extends AbstractTableModel {
     HashMap<Integer, JTextComponent> filterFields = new HashMap<>();
     protected final ArrayList<T> rows = new ArrayList<>();
+    private boolean isEditable = true;
+
+    private String[] cols = {};
+
+
+    protected AbstractTableModelWithFilters() {
+    }
+
+
+    @Override
+    public int getRowCount() {
+        return rows.size();
+    }
+
+    @Override
+    public String getColumnName(int column) { return cols[column]; }
+
+    public void addRow(T item) {
+        rows.add(item);
+        fireTableRowsInserted(rows.size()-2, rows.size());
+    }
+
+    @Override
+    public int getColumnCount() {
+        return cols.length;
+    }
+
+    public String[] getCols() {
+        return cols;
+    }
+
+    public void setCols(String[] cols) {
+        this.cols = cols;
+    }
+
+
+
+    public ArrayList<T> getRows() {
+        return rows;
+    }
 
     public void setFilterField(int column, JTextComponent filterField) {
         filterFields.put(column, filterField);
@@ -28,6 +69,16 @@ public abstract class AbstractTableModelWithFilters<T> extends AbstractTableMode
         filterFields.clear();
     }
 
+    public boolean isEditable() {
+        return isEditable;
+    }
+    public void setEditable(boolean editable) {
+        isEditable = editable;
+    }
+
+
+    public boolean isCellEditable(int row, int column) { return isEditable;}
+
     public RowFilter<Object, Object> createTableFilter() {
         ArrayList<RowFilter<Object, Object>> filters = new ArrayList<>();
         var keyList = filterFields.keySet().stream().toList();
@@ -38,7 +89,19 @@ public abstract class AbstractTableModelWithFilters<T> extends AbstractTableMode
         return RowFilter.andFilter(filters);
     }
 
-    public abstract List<?> getColumnValue(int col);
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        var item = rows.get(rowIndex);
+        return translateValue(item, columnIndex);
+    }
 
-    public abstract List<String> getColumnValueToString(int col);
+    public abstract Object translateValue(T value, int columnIndex);
+
+    public List<?> getColumnValue(int col) {
+        return rows.stream().map((book) -> translateValue(book, col)).toList();
+    };
+
+    public List<String> getColumnValueToString(int col) {
+        return rows.stream().map(book -> Objects.toString(translateValue(book, col))).toList();
+    };
 }
