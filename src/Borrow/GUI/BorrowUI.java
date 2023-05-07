071a65588987fbe652a2d060b4d13c8a7a57880c
thevu29 <vnguyen132az@gmail.com>
2023-05-07 15:26:04 +0700

    borrow done

--- src/Borrow/GUI/BorrowUI.java
+++ src/Borrow/GUI/BorrowUI.java
@@ -289,8 +289,8 @@ public class BorrowUI {
 
                 String id = borrowTable.getValueAt(selectedRow, 0).toString();
 
-                if (!borrowModel.checkBorrow(id)) {
-                    JOptionPane.showMessageDialog(null, "Phiếu mượn chưa đc trả", "Warning",
+                if (borrowModel.checkBorrow(id)) {
+                    JOptionPane.showMessageDialog(null, "Phiếu đã được xác nhận trả!", "Warning",
                             JOptionPane.WARNING_MESSAGE);
                     return;
                 }
@@ -316,7 +316,7 @@ public class BorrowUI {
                 String id = borrowTable.getValueAt(selectedRow, 0).toString();
 
                 if (borrowModel.checkBorrow(id)) {
-                    JOptionPane.showMessageDialog(null, "Phiếu đã đươc xác nhận trả!", "Warning",
+                    JOptionPane.showMessageDialog(null, "Phiếu đã được xác nhận trả!", "Warning",
                             JOptionPane.WARNING_MESSAGE);
                     return;
                 }
